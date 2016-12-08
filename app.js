2705be57ac01b1009827da13a01ec244a761a512
Ricardo <rick@reddraft.com>
2016-12-08 17:28:32 -0600

    Create methods to get input from DOM in the UIController

--- app.js
+++ app.js
@@ -13,7 +13,29 @@ var budgetController = (function() {
 
 var UIController = (function() {
 
-  // Some Code
+  // DOM Elements
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  // return UIController Object
+  return {
+      // getInput() returns an object with type.value description.value value.value as properties
+      getInput: function() {
+        return {
+          type: document.querySelector(DOMstrings.inputType).value,// get value="income" or value="expense" from html
+          description: document.querySelector(DOMstrings.inputDescription).value,
+          value: document.querySelector(DOMstrings.inputValue).valueAsNumber
+        };
+      },
+      getDOMstrings: function() {
+        return DOMstrings;
+      }
+
+  }
 
 })();
 
@@ -22,10 +44,12 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function() {
 
     // 1. get the field input  data
-
+    var input = UICtrl.getInput();
     // 2. add item to the budget controller
 
     // 3. add the item to the user interface
@@ -33,10 +57,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 4. calculate the budget
 
     // 5. Display the budget in the UI
-  
+
   };
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 
   document.addEventListener('keypress', function(event) {
