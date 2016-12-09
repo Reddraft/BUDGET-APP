6d8693322757abf21441d7e7d07ecfd20ea0ce31
Ricardo <rick@reddraft.com>
2016-12-08 19:39:41 -0600

    Create setupListeners initiliazer Function in the controller

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
-
-//======= BUDGET CONTROLLER ==============
+/**********************************************
+*** BUDGET CONTROLLER
+**********************************************/
 
 var budgetController = (function() {
 
@@ -9,11 +10,14 @@ var budgetController = (function() {
 })();
 
 
-//======= UI CONTROLLER ==============
+
+/**********************************************
+*** UI CONTROLLER
+**********************************************/
 
 var UIController = (function() {
 
-  // DOM Elements
+  // DOM ELEMENTS OBJECT VARIABLE
   var DOMstrings = {
     inputType: '.add__type',
     inputDescription: '.add__description',
@@ -21,9 +25,9 @@ var UIController = (function() {
     inputBtn: '.add__btn'
   }
 
-  // return UIController Object
+  //---RETURN UICONTROLLER OBJECT
   return {
-      // getInput() returns an object with type.value description.value value.value as properties
+      //getInput() returns an object with type.value description.value value.value as properties
       getInput: function() {
         return {
           type: document.querySelector(DOMstrings.inputType).value,// get value="income" or value="expense" from html
@@ -31,6 +35,7 @@ var UIController = (function() {
           value: document.querySelector(DOMstrings.inputValue).valueAsNumber
         };
       },
+      //getDOMstrings() returns the DOMstrings object variable
       getDOMstrings: function() {
         return DOMstrings;
       }
@@ -40,15 +45,34 @@ var UIController = (function() {
 })();
 
 
-//======= APP CONTROLLER ==============
+/**********************************************
+*** APP CONTROLLER
+**********************************************/
 
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var DOM = UICtrl.getDOMstrings();
+  // ---- SETUP EVENT LISTENERS FUNCTION
+  var setupEventListeners = function() {
 
+    //call UICtrl.getDOMstrings() to access DOM elements from the UIController
+    var DOM = UICtrl.getDOMstrings();
+
+    //execute ctrlAddItem() when click DOM.inputBtn
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    //execute ctrlAddItem() when click "Enter" Key
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+          ctrlAddItem();
+      }
+    });
+  };
+
+
+  //----ADD ITEM FUNCTION
   var ctrlAddItem = function() {
 
-    // 1. get the field input  data
+    // 1. get the field input data from the UIController
     var input = UICtrl.getInput();
     // 2. add item to the budget controller
 
@@ -60,18 +84,16 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   };
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-
-  document.addEventListener('keypress', function(event) {
-
-    if (event.keyCode === 13 || event.which === 13) {
-
-        ctrlAddItem();
-
+  //--- RETURN CONTROLLER OBJECT
+  return {
+    init: function() {
+        setupEventListeners();
     }
-
-  });
-
+  }
 
 })(budgetController, UIController);
+
+/**********************************************
+*** INITIALIZATION
+**********************************************/
+controller.init();
