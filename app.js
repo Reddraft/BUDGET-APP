b62ef77ed5662378f47abd46f230a85efdab2cb2
Ricardo <rick@reddraft.com>
2016-12-12 14:22:05 -0600

    Create calculateBudget and getBusget methods  in the Budget controller then call it in the app controller

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var budgetController = (function() {
       this.value = value;
   };
 
+
   // RETRIEVE DATA STRUCTURE OBJECT
   var data = {
     allItems: {
@@ -28,8 +29,23 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
+
+  // CALCULATE TOTAL FUNCTION
+  var calculateTotal = function(type) {
+    //initialize sum with 0
+    var sum = 0;
+    //depending on the type loop through all items in the array then sum all its values
+    data.allItems[type].forEach(function(item) {
+      sum += item.value;
+    });
+    // set the total of the sum of all items value in the array
+    data.totals[type] = sum;
+  };
+
 
   //---RETURN BUDGETCONTROLLER OBJECT
   return {
@@ -62,6 +78,35 @@ var budgetController = (function() {
       data.allItems[type].push(newItem);
       // return new item
       return newItem;
+    },
+
+    //---calculateBudget() calculate based on icomes and expnses what's tha final budget
+    calculateBudget: function() {
+      //calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+      //calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+      //calculte the percentage of income that was spent
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round( (data.totals.exp / data.totals.inc) * 100 );
+      } else {
+        data.percentage = -1;
+      }
+
+    },
+    //---getBudget() returns an object with ev
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      }
+    },
+
+    testing: function() {
+      console.log(data);
     }
 
   };
@@ -93,7 +138,7 @@ var uiController = (function() {
         return {
           type: document.querySelector(domStrings.inputType).value,// get value="inc" or value="exp" from html
           description: document.querySelector(domStrings.inputDescription).value,
-          value: document.querySelector(domStrings.inputValue).valueAsNumber
+          value: parseFloat(document.querySelector(domStrings.inputValue).value)
         };
       },
 
@@ -174,26 +219,38 @@ var uiController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
+  //---UPDATE BUDGET FUNCTION
+  var updateBudget = function() {
+    // 1. calculate the budget
+    budgetCtrl.calculateBudget();
+    // 2. return the budget
+    var budget = budgetCtrl.getBudget();
+    // 3. Display the budget in the UI
+    console.log(budget);
+  }
+
   //---ADD ITEM FUNCTION
   var ctrlAddItem = function() {
 
     var input, newItem;
-    // 1. get the field input data from the uiController
+    // 1. call UICtrl.getInput() to get the field input data from the uiController
     input = UICtrl.getInput();
-    // 2. add item to the budget controller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-    // 3. add the item to the user interface and then  clear the input fields
-    uiController.addListItem(newItem, input.type);
-    uiController.clearFields();
-    // 4. calculate the budget
-
-    // 5. Display the budget in the UI
+    //if there's a description, the value is a number and the value is not 0. Execute the rest of the steps
+    if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+     // 2. call budgetCtrl.additem() to add an item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+      // 3. call uiController.addListItem() to add the item to the user interface
+      uiController.addListItem(newItem, input.type);
+      //4 clear the input fields
+      uiController.clearFields();
+      //5 Calculate and update the budget
+      updateBudget();
+    }
 
   };
 
   // ---- SETUP EVENT LISTENERS FUNCTION
   var setupEventListeners = function() {
-
 
     //call UICtrl.getDOMstrings() to access DOM elements from the uiController
     var DOM = UICtrl.getDOMstrings();
